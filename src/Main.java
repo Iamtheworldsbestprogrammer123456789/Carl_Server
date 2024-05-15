f0c80fc54411899d5023cc00841274e6892b794a
carl.hummerhielm <carl.hummerhielm@elev.ga.ntig.se>
2024-05-15 10:20:30 +0200

    håller på...

--- src/Main.java
+++ src/Main.java
@@ -26,10 +26,44 @@ public class Main {
                 serverMulticastSocket.receive(data);
                 String msg = new String(data.getData()).trim();
                 System.out.println("Message recived from client = " + msg);
+                double resultat = räknare(msg);
+                data = new DatagramPacket(msg.getBytes(), 0, msg.length(), group, portnumber);
+                serverMulticastSocket.send(data);
+
             }
             serverMulticastSocket.close();
         }catch (IOException ie){
             ie.printStackTrace();
+        }
+    }
+    private static double räknare(String matteTal){
+        if(matteTal.contains("+")){
+            String[] tal =matteTal.split("+".toString());
+            int tal1 = Integer.parseInt(tal[0]);
+            int tal2 = Integer.parseInt(tal[1]);
+            return tal1+tal2;
+        }
+        else if(matteTal.contains("-")){
+            String[] tal =matteTal.split("-");
+            int tal1 = Integer.parseInt(tal[0]);
+            int tal2 = Integer.parseInt(tal[1]);
+            return tal1-tal2;
+        }
+        else if(matteTal.contains("*")){
+            String[] tal =matteTal.split("x");
+            int tal1 = Integer.parseInt(tal[0]);
+            int tal2 = Integer.parseInt(tal[1]);
+            return tal1*tal2;
+        }
+        else if(matteTal.contains("/")){
+            String[] tal =matteTal.split("/");
+            int tal1 = Integer.parseInt(tal[0]);
+            int tal2 = Integer.parseInt(tal[1]);
+            return tal1/tal2;
+        }
+        else {
+            System.out.println("Invalid input...");
+            return 0;
         }
     }
 }
