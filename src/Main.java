5568dc66a99702c17f8768400184a1c77082f81a
carl.hummerhielm <carl.hummerhielm@elev.ga.ntig.se>
2024-05-15 09:56:40 +0200

    Börjar med koden från multicastserver

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,35 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+        int portnumber = 8880;
+        if (args.length >= 1){
+            portnumber = Integer.parseInt(args[0]);
+        }
+        try{
+            MulticastSocket serverMulticastSocket = new MulticastSocket(portnumber);
+
+            InetAddress group = InetAddress.getByName("224.4.5.6");
+
+            serverMulticastSocket.joinGroup(group);
+            System.out.println("joinGroup method is called...");
+            boolean infinite = true;
+
+            while(infinite){
+                byte buf[] = new byte[1024];
+                DatagramPacket data = new DatagramPacket(buf, buf.length);
+                serverMulticastSocket.receive(data);
+                String msg = new String(data.getData()).trim();
+                System.out.println("Message recived from client = " + msg);
+            }
+            serverMulticastSocket.close();
+        }catch (IOException ie){
+            ie.printStackTrace();
+        }
     }
 }
