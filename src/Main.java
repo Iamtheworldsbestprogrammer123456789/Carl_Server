255734ec7888195733890f0a9126ab8387b5d50e
carl.hummerhielm <carl.hummerhielm@elev.ga.ntig.se>
2024-05-16 16:45:27 +0200

    Har putsat till koden lite och lagt till kommentarer

--- src/Main.java
+++ src/Main.java
@@ -7,61 +7,72 @@ public class Main {
     public static void main(String[] args) {
 
         int portnumber = 8880;
-        if (args.length >= 1){
+        if (args.length >= 1) {
             portnumber = Integer.parseInt(args[0]);
         }
-        try{
+        try {
             MulticastSocket serverMulticastSocket = new MulticastSocket(portnumber);
-
             InetAddress group = InetAddress.getByName("224.4.5.6");
 
             serverMulticastSocket.joinGroup(group);
             System.out.println("joinGroup method is called...");
-            boolean infinite = true;
-
-                byte buf[] = new byte[1024];
-                DatagramPacket data = new DatagramPacket(buf, buf.length);
-                serverMulticastSocket.receive(data);
-                String msg = new String(data.getData()).trim();
-                String resultat = räknare(msg);
-                System.out.println("Resultat: " + resultat);
-                DatagramPacket resultatet = new DatagramPacket(resultat.getBytes(), resultat.getBytes().length, group, portnumber);
-                serverMulticastSocket.send(resultatet);
-
+            byte buf[] = new byte[1024];
+            //Tar emot datan
+            DatagramPacket data = new DatagramPacket(buf, buf.length);
+            serverMulticastSocket.receive(data);
+            String msg = new String(data.getData()).trim();
+            //Räkar ut vad svaret blir
+            String resultat = räknare(msg);
+            //Skickar datan
+            DatagramPacket resultatet = new DatagramPacket(resultat.getBytes(), resultat.getBytes().length, group, portnumber);
+            serverMulticastSocket.send(resultatet);
+            //Stänger socketen
             serverMulticastSocket.close();
-        }catch (IOException ie){
+        } catch (IOException ie) {
             ie.printStackTrace();
         }
     }
-    private static String räknare(String matteTal){
-        if(matteTal.contains("+")){
-            String[] tal =matteTal.split("\\+");
+
+    //Räknar ut svaret på enkla matte tal och skickar tillbaka svaret som en string
+    private static String räknare(String matteTal) {
+
+        //Addition
+        if (matteTal.contains("+")) {
+            String[] tal = matteTal.split("\\+");
             int tal1 = Integer.parseInt(tal[0]);
             int tal2 = Integer.parseInt(tal[1]);
-            String talet = String.valueOf(tal1+tal2);
-            return tal[0] + "+"+ tal[1] + " = " + talet;
+            String talet = String.valueOf(tal1 + tal2);
+            return tal[0] + "+" + tal[1] + " = " + talet;
         }
-        else if(matteTal.contains("-")){
-            String[] tal =matteTal.split("-");
+
+        //Subtraktion
+        else if (matteTal.contains("-")) {
+            String[] tal = matteTal.split("-");
             int tal1 = Integer.parseInt(tal[0]);
             int tal2 = Integer.parseInt(tal[1]);
-            String talet = String.valueOf(tal1-tal2);
-            return tal[0] + "-"+ tal[1] + " = " + talet;
+            String talet = String.valueOf(tal1 - tal2);
+            return tal[0] + "-" + tal[1] + " = " + talet;
         }
-        else if(matteTal.contains("*")){
-            String[] tal =matteTal.split("\\*");
+
+        //Multiplikation
+        else if (matteTal.contains("*")) {
+            String[] tal = matteTal.split("\\*");
             int tal1 = Integer.parseInt(tal[0]);
             int tal2 = Integer.parseInt(tal[1]);
-            String talet = String.valueOf(tal1*tal2);
-            return tal[0] + "*"+ tal[1] + " = " + talet;
+            String talet = String.valueOf(tal1 * tal2);
+            return tal[0] + "*" + tal[1] + " = " + talet;
         }
-        else if(matteTal.contains("/")){
-            String[] tal =matteTal.split("/");
+
+        //Division
+        else if (matteTal.contains("/")) {
+            String[] tal = matteTal.split("/");
             int tal1 = Integer.parseInt(tal[0]);
             int tal2 = Integer.parseInt(tal[1]);
-            String talet = String.valueOf(tal1/tal2);
-            return tal[0] + "/"+ tal[1] + " = " + talet;
+            String talet = String.valueOf(tal1 / tal2);
+            return tal[0] + "/" + tal[1] + " = " + talet;
         }
+
+        //Om inte metoden känner av någon känd matte operatör så skickas detta tillbaka
         else {
             return "Fel input";
         }
